9663355c8ac8d89c25ac3f530b60393aef83e0d2
juddy.dev <contacto@juddy.dev>
2023-12-08 21:32:00 -0500

    init

---
+++ estimaciones/estimaciones/src/main/java/dev/juddy/estimaciones/EstimacionesApplication.java
@@ -0,0 +1,13 @@
+package dev.juddy.estimaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimacionesApplication.class, args);
+	}
+
+}
---
+++ estimaciones/estimaciones/src/test/java/dev/juddy/estimaciones/EstimacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juddy.estimaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
